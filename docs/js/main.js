dce9ed21ba07b8a3dffffc6ef2a5465cbff3513c
AntonioMateoGomez <antonio.mateo@47deg.com>
2020-05-05 19:43:28 +0200

    Basic website structure

---
+++ docs/js/main.js
@@ -0,0 +1,75 @@
+$(function() {
+
+  // NAV POSITION
+  var navPos = $('nav').position().top;
+  var lastPos = 0;
+  var lockTimer;
+
+  $(window).on('scroll', function() {
+    var pos = $(window).scrollTop();
+    var pos2 = pos + 50;
+    var scrollBottom = pos + $(window).height();
+
+    if (pos >= navPos + $('nav').height() && lastPos < pos) {
+      $('nav').addClass('fixed');
+    }
+    if (pos < navPos && lastPos > pos) {
+      $('nav').removeClass('fixed');
+    }
+    lastPos = pos;
+
+    // Link Highlighting
+    if (pos2 > $('#home').offset().top) {
+      highlightLink('home');
+    }
+    if (pos2 > $('#bio').offset().top) {
+      highlightLink('bio');
+    }
+    if (pos2 > $('#portfolio').offset().top) {
+      highlightLink('portfolio');
+    }
+    if (
+      pos2 > $('#contact').offset().top ||
+      pos + $(window).height() === $(document).height()
+    ) {
+      highlightLink('contact');
+    }
+
+    // Prevent Hover on Scroll
+    clearTimeout(lockTimer);
+    if (!$('body').hasClass('disable-hover')) {
+      $('body').addClass('disable-hover');
+    }
+
+    lockTimer = setTimeout(function() {
+      $('body').removeClass('disable-hover');
+    }, 500);
+
+  });
+
+  function highlightLink(anchor) {
+    $('nav .active').removeClass('active');
+    $('nav')
+      .find('[dest="' + anchor + '"]')
+      .addClass('active');
+  }
+
+  // EVENT HANDLERS
+  $('.page-link').click(function() {
+    var anchor = $(this).attr('dest');
+    $('.link-wrap').removeClass('visible');
+
+    $('nav span').removeClass('active');
+    $('nav')
+      .find('[dest="' + anchor + '"]')
+      .addClass('active');
+
+    $('html, body').animate(
+      {
+        scrollTop: $('#' + anchor).offset().top
+      },
+      400
+    );
+  });
+
+});
